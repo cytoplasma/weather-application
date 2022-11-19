b94f9a0ebf9017d030f55e521e04590166c02c4d
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-11-18 18:39:59 -0800

    redo aside/navbar with bootstrap, setup CSS root variables, setup JS variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
-var APIKey = 
+var searchHistory = [];
+var APIKey = '13aeebf93c9547e3a573810e1a64c2b2';
+var lat = city.coord.lat;
+var lon = city.coord.lon;
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
-var city;
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
+var searchForm = document.querySelector("#search-form");
+var searchInput = document.querySelector("#search-input");
+var searchHistory = document.querySelector("#history");
