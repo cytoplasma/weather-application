70b22dd29098ec1e0ae2eb735943a70bf612f091
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-11-23 22:18:04 -0800

    debugging JS; only returns MOSCOW RUSSIA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var APIKey = '13aeebf93c9547e3a573810e1a64c2b2';
-var APIcall = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=13aeebf93c9547e3a573810e1a64c2b2";
+var APIcall = "http://api.openweathermap.org/data/2.5/forecast?&units=imperial&id=524901&appid=13aeebf93c9547e3a573810e1a64c2b2";
 // var queryURL = 
 // "https://api.openweathermap.org/data/2.5/forecast?"
 
@@ -7,7 +7,7 @@ var cityName = document.querySelector("#search-city");
 var cityQuery = cityName.value;
 cityQuery = cityQuery.replace(" ", "_");
 // var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityQuery + "&appid=" + APIKey;
-var geoURL = "http://api.openweathermap.org/geo/1.0/direct?appid=13aeebf93c9547e3a573810e1a64c2b2&q="
+var geoURL = "http://api.openweathermap.org/geo/1.0/direct?&appid=13aeebf93c9547e3a573810e1a64c2b2&q="
 var searchBtn = document.querySelector("#search-button");
 var searchQuery = document.querySelector("#search-city");
 
@@ -21,14 +21,21 @@ function fetchWeather() {
         return response.json();
     })
     .then(function (data){
-        console.log(data);
+        // console.log(data);
         var lat = "&lat=" + data[0].lat;
         var lon = "&lat=" + data[0].lon;
-        return fetch(APIcall + lat + lon);
+        console.log(APIcall + lat + lon);
+        console.log(data[0].lat);
+        // return fetch(APIcall + lat + lon);
     })
-    .then(function(response) {
-        return response.json;
-    })
+    // .then(function (response) {
+    //     // return response.json();
+    // })
+    // .then(function(data) {
+    //     // console.log(data);
+    //     document.getElementById("cityName").textContent = userInput + "'s weather for today."
+    //     document.getElementById("cityTempCur").textContent = data.list[3].main.temp;
+    // });
 }
 
 
