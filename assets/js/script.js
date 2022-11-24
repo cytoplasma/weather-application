719d1845d25bc6b0b89a4b3c6dff01ef7d5b15af
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-11-23 21:26:04 -0800

    JS: when search query is submitted, the correct data is returned

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,36 @@
 var APIKey = '13aeebf93c9547e3a573810e1a64c2b2';
-var APIcall = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=' + APIKey;
-var queryURL = 
-"https://api.openweathermap.org/data/2.5/forecast?"
+var APIcall = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=13aeebf93c9547e3a573810e1a64c2b2";
+// var queryURL = 
+// "https://api.openweathermap.org/data/2.5/forecast?"
 
 var cityName = document.querySelector("#search-city");
 var cityQuery = cityName.value;
 cityQuery = cityQuery.replace(" ", "_");
-var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityQuery + "&appid=" + APIKey;
-
+// var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityQuery + "&appid=" + APIKey;
+var geoURL = "http://api.openweathermap.org/geo/1.0/direct?appid=13aeebf93c9547e3a573810e1a64c2b2&q="
 var searchBtn = document.querySelector("#search-button");
 var searchQuery = document.querySelector("#search-city");
 
 
 function fetchWeather() {
+
     userInput = searchQuery.value;
 
-    fetch(APIcall)
-    .then (function(res) {
-        return res.json();
-    }) 
-    .then (function(data) {
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-        var currentQuery = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=13aeebf93c9547e3a573810e1a64c2b2&units=${cityQuery}`;
-        return fetch(currentQuery);
+    fetch(geoURL + userInput) 
+    .then(function (response){
+        return response.json();
     })
-    .then (function(res) {
-        return res.json();
+    .then(function (data){
+        console.log(data);
+        var lat = "&lat=" + data[0].lat;
+        var lon = "&lat=" + data[0].lon;
+        return fetch(APIcall + lat + lon);
+    })
+    .then(function(response) {
+        return response.json;
     })
 }
+
+
 
 searchBtn.addEventListener("click", fetchWeather);
